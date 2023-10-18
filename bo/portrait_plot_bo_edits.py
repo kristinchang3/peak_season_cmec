41188eceb1c477648cf7caac66a8301446eb6b8e
Bo <130708142+bosup@users.noreply.github.com>
2023-10-18 15:25:03 -0700

    Update portrait_plot_bo_edits.py

--- bo/portrait_plot_bo_edits.py
+++ bo/portrait_plot_bo_edits.py
@@ -104,8 +104,8 @@ cell_height = 1/num_models
 cell_width = 1/num_regions
 
 # set desired figure size
-desired_width = 2000
-desired_height = 2000
+desired_width = 5000
+desired_height = 5000
 
 # calculate final width and height based on above parameters
 adjusted_width = round(int(cell_width * desired_width), -1)
