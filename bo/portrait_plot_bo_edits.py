da9e5e9c65277ad7a410112d9807c0c0cd3c25fe
bosup <dabocn@hotmail.com>
2023-10-18 21:12:45 -0700

    modified:   bo_charts/peak_plot25.html 	modified:   bo_charts/peak_plot26.html 	modified:   portrait_plot_bo_edits.py 	modified:   portrait_plot_reanalysis.py

--- bo/portrait_plot_bo_edits.py
+++ bo/portrait_plot_bo_edits.py
@@ -54,7 +54,7 @@ ddd = dd['peak'].values
 #print(dd)
 
 #img_path = 'https://raw.githubusercontent.com/kristinchang3/peak_season_cmec/main/images/'
-img_path = 'https://raw.githubusercontent.com/kristinchang3/peak_season_cmec/main/bo/bo_images/'
+img_path = 'https://raw.githubusercontent.com/kristinchang3/peak_season_cmec/bo/bo_images/'
 img_links = []
 
 for i, model in enumerate(model_names):
